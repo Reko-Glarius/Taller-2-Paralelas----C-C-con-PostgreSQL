e8319d383412fe440b849a4ecc8c90111af3f558
Ricardo Aliste G <39688076+Reko-Glarius@users.noreply.github.com>
2020-05-08 13:52:14 -0400

    Update main.cpp

--- simulador-omp-pg/main.cpp
+++ simulador-omp-pg/main.cpp
@@ -9,21 +9,23 @@ int main(int argc, char** argv)
 {
     int ,n_campos,n_filas,promedio=0,dato=0;
     
-    ofstream archivo ("Promedio.csv");
+    std::ofstream archivo ("Promedio.csv");
     PGconn* conexion = dbconnect((char *) DBSERVER, DBPORT, (char *) DBNAME, (char *) DBUSER, (char *) DBPASSWORD);
     std:: string query="SELECT rut,nem,ranking,matematicas,lenguaje,ciencias,historia FROM puntajes ORDER BY rut");
     PGresult* resultado = dbquery(conexion, (char *) query.c_str());
     
     for (i=0; i<PQnTuples(resultado);i++)
     {
-        for(j=2; j<PQnfields(resultado;j++)
+        for(j=1; j<PQnfields(resultado;j++)
         {
             std::istringstream(PQgetvalue(resultado, i, j)) >> dato;
             promedio+=dato;
+            dato=0;
         }
         promedio=(promedio/6);
-        archivo<<PQgetvalue(resultado, i, 1))<<";"<<promedio<<endl;
+        archivo<<PQgetvalue(resultado, i, 0))<<";"<<promedio<<endl;
         promedio=0;
     }
     dbclose(conexion);
+    archivo.close();
 }
